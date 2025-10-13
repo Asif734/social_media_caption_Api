bc29797639855c01157d4fc8b69eda6087e39f1c
Asifuzzaman Asif <asifuzzamanasif734@gmail.com>
2025-10-14 04:58:39 +0600

    Upadated

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+
+
 from fastapi import FastAPI
 from app.api.v1.api import api_router
 
 app = FastAPI(title="Caption Generator")
 
+@api_router.get("/")
+def api_root():
+    return {"message": "API v1 working"}
 # Register API routes
 app.include_router(api_router, prefix="/api/v1")
-
